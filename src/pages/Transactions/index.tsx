092df3f8690befe5b78b200e1e8d821db3277aa1
Robson Santos <rboxe68@gmail.com>
2024-02-17 19:10:24 -0300

    Atualizado

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components/Header"
+import { Summary } from "../../components/Summary"
+
+export const Transactions = () => {
+  return (
+    <div>
+        <Header />
+        <Summary />
+    </div>
+  )
+}
+
+ 
