e321a38ca32109a053af46c6c51f087bcdeda5c1
Robson Santos <rboxe68@gmail.com>
2024-02-21 23:57:50 -0300

    Atualizado

---
+++ src/pages/Transactions/components/searchForm/index.tsx
@@ -0,0 +1,15 @@
+import { MagnifyingGlass } from "phosphor-react"
+import { SearchFormContainer } from "./styles"
+
+export const SearchForm = () => {
+  return (
+    <SearchFormContainer>
+        <input type="text" placeholder="Busque por transações" />
+
+        <button type="submit">
+            <MagnifyingGlass size={20} />
+        </button>
+    </SearchFormContainer>
+  )
+}
+
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,42 @@
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
+import { SearchForm } from "./components/searchForm"
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
 
 export const Transactions = () => {
   return (
     <div>
         <Header />
         <Summary />
+
+       <TransactionsContainer>
+          <SearchForm />
+
+       <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="incone">
+                  R$ 12.000,00 
+                </PriceHighlight>               
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hanburger</td>
+              <td>
+              <PriceHighlight variant="outcone">
+                - R$ 59,00
+              </PriceHighlight>                
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2023</td>
+            </tr>          
+          </tbody>
+        </TransactionsTable>
+       </TransactionsContainer>
     </div>
   )
 }
