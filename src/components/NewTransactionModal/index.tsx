5ed9325133f63861723b90af9a0e04917ae3df0b
Robson Santos <rboxe68@gmail.com>
2024-02-26 23:00:41 -0300

    Atualizado

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,32 +1,46 @@
-import * as Dialog from '@radix-ui/react-dialog';
-import { X } from 'phosphor-react';
+import * as Dialog from "@radix-ui/react-dialog";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
-import { CloseButton, Content, Overley } from './styles';
+import {
+    CloseButton,
+    Content,
+    Overley,
+    TransactionType,
+    TransactionTypeButton,
+} from "./styles";
 
 export const NewTransactionModal = () => {
-  return (
-    <Dialog.Portal>
+    return (
+        <Dialog.Portal>
             <Overley />
 
-        <Content>
-            <Dialog.Title>Nova transação</Dialog.Title>
+            <Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
 
-            <CloseButton>
-                <X size={24} />
-            </CloseButton>
+                <CloseButton>
+                    <X size={24} />
+                </CloseButton>
 
-            <form action="">
-                <input type="text" placeholder='Descrição' required />
-                <input type="number" placeholder='Preço' required />
-                <input type="text" placeholder='Categoria' required />
+                <form action="">
+                    <input type="text" placeholder="Descrição" required />
+                    <input type="number" placeholder="Preço" required />
+                    <input type="text" placeholder="Categoria" required />
 
-                <button type="submit">
-                    Cadastrar
-                </button>
-            </form>
-        
-        </Content>
-    </Dialog.Portal>
-  )
-}
+                    <TransactionType>
+                        <TransactionTypeButton variant="incone" value="incone">
+                            <ArrowCircleUp size={24} />
+                            Entrada
+                        </TransactionTypeButton>
 
+                        <TransactionTypeButton variant="outcone" value="outcone">
+                            <ArrowCircleDown size={24} />
+                            Saída
+                        </TransactionTypeButton>
+                    </TransactionType>
+
+                    <button type="submit">Cadastrar</button>
+                </form>
+            </Content>
+        </Dialog.Portal>
+    );
+};
