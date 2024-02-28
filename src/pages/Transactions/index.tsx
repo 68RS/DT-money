6d157dc214478f6816cf3bc5550dc8365c55acd4
Robson Santos <rboxe68@gmail.com>
2024-02-27 23:23:58 -0300

    Atualizado

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,44 +1,63 @@
-import { Header } from "../../components/Header"
-import { Summary } from "../../components/Summary"
-import { SearchForm } from "./components/searchForm"
-import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
+import { useEffect, useState } from "react";
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/searchForm";
+import {
+    PriceHighlight,
+    TransactionsContainer,
+    TransactionsTable,
+} from "./styles";
 
-export const Transactions = () => {
-  return (
-    <div>
-        <Header />
-        <Summary />
-
-       <TransactionsContainer>
-          <SearchForm />
-
-        <TransactionsTable>
-            <tbody>
-              <tr>
-                <td width="50%">Desenvolvimento de site</td>
-                <td>
-                  <PriceHighlight variant="incone">
-                    R$ 12.000,00 
-                  </PriceHighlight>               
-                </td>
-                <td>Venda</td>
-                <td>13/04/2023</td>
-              </tr>
-              <tr>
-                <td width="50%">Hanburger</td>
-                <td>
-                <PriceHighlight variant="outcone">
-                  - R$ 59,00
-                </PriceHighlight>                
-                </td>
-                <td>Alimentação</td>
-                <td>10/04/2023</td>
-              </tr>          
-            </tbody>
-          </TransactionsTable>
-       </TransactionsContainer>
-    </div>
-  )
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'incone' | 'outcone';
+  price: number;
+  category: string;
+  createdAT: string;
 }
 
- 
+export const Transactions = () => {
+    const [Transactions, setTransactions] = useState<Transaction[]>([])
+
+    async function loadTransactions() {
+        const response = await fetch("http://localhost:3000/transactions");
+        const data = await response.json();
+
+        setTransactions(data);
+    }
+    useEffect(() => {
+        loadTransactions();
+    }, []);
+
+    return (
+        <div>
+            <Header />
+            <Summary />
+
+            <TransactionsContainer>
+                <SearchForm />
+
+                <TransactionsTable>
+                    <tbody>
+                      {Transactions.map(transaction => {
+                        return (
+                          <tr key={transaction.id}>
+                            <td width="50%">{transaction.description}</td>
+                            <td>
+                                <PriceHighlight variant={transaction.type}>
+                                    {transaction.price}
+                                </PriceHighlight>
+                            </td>
+                            <td>{transaction.category}</td>
+                            <td>{transaction.createdAT}</td>
+                        </tr>
+                        )
+                      } )}                        
+                       
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
+        </div>
+    );
+};
